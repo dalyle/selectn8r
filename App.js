500203a005d9c5084c2d85e9020778fc93b87996
Greg Stevens <greg@dalyle.ca>
2022-09-04 18:39:27 -0600

    fix(react): 🐛 try2: Fix warning from React 18 about behaving as 17

--- App.js
+++ App.js
@@ -2,10 +2,15 @@
 
 import React from 'react';
 import {StyleSheet, Text, View, Image} from 'react-native';
+// React 18 from 17 requires `root.render(...)`
+import { createRoot } from 'react-dom/client';
 
 // Load Data - `cards` and `cards2`
 import { cards, cards2 } from './data/dogs.js';
 import SwipeCards from 'react-native-swipe-cards';
+
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
 
 class Card extends React.Component {
   constructor(props) {
@@ -28,7 +33,7 @@ class NoMoreCards extends React.Component {
   }
 
   render() {
-    return (
+    return render(
       <View style={styles.noMoreCards}>
         <Text>No more cards</Text>
       </View>
@@ -36,30 +41,6 @@ class NoMoreCards extends React.Component {
   }
 }
 
-const cards = [
-  {name: 'Dog Clean Tub', image: 'https://media.giphy.com/media/GfXFVHUzjlbOg/giphy.gif'},
-  {name: '2', image: 'https://media.giphy.com/media/irTuv1L1T34TC/giphy.gif'},
-  {name: '3', image: 'https://media.giphy.com/media/LkLL0HJerdXMI/giphy.gif'},
-  {name: '4', image: 'https://media.giphy.com/media/fFBmUMzFL5zRS/giphy.gif'},
-  {name: '5', image: 'https://media.giphy.com/media/oDLDbBgf0dkis/giphy.gif'},
-  {name: '6', image: 'https://media.giphy.com/media/7r4g8V2UkBUcw/giphy.gif'},
-  {name: '7', image: 'https://media.giphy.com/media/K6Q7ZCdLy8pCE/giphy.gif'},
-  {name: '8', image: 'https://media.giphy.com/media/hEwST9KM0UGti/giphy.gif'},
-  {name: '9', image: 'https://media.giphy.com/media/3oEduJbDtIuA2VrtS0/giphy.gif'},
-]
-
-const cards2 = [
-  {name: '10', image: 'https://media.giphy.com/media/12b3E4U9aSndxC/giphy.gif'},
-  {name: '11', image: 'https://media4.giphy.com/media/6csVEPEmHWhWg/200.gif'},
-  {name: '12', image: 'https://media4.giphy.com/media/AA69fOAMCPa4o/200.gif'},
-  {name: '13', image: 'https://media.giphy.com/media/OVHFny0I7njuU/giphy.gif'},
-]
-
-import { createRoot } from 'react-dom/client';
-const container = document.getElementById('root');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-
-root.render(<App />);
 
 
 export default class App extends React.Component {
@@ -105,7 +86,7 @@ export default class App extends React.Component {
 
 
 
-    return root.render(
+    return (
       <SwipeCards
         cards={this.state.cards}
         loop={false}
@@ -148,3 +129,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   }
 })
+
+
+root.render(<App />);
